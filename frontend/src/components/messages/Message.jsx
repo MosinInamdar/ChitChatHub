8cbb031040b84809d9659eba7c99242092ebb0cc
MosinInamdar <mosininamdar18@gmail.com>
2024-02-20 07:42:05 +0530

    2nd Part Completed- ui design

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Message = () => {
+  return (
+    <div className="chat chat-end">
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img
+            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+            alt="Avatar"
+          />
+        </div>
+      </div>
+      <div className={`chat-bubble text-white bg-blue-500`}>Hi, What's up?</div>
+      <div className="chat-footer opacity-50">Seen at 12:46</div>
+    </div>
+  );
+};
+
+export default Message;
