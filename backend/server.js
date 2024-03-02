47af753c3d43316cf70cac23db39fe708177f398
MosinInamdar <mosininamdar18@gmail.com>
2024-03-02 08:19:14 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -11,6 +11,7 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 dotenv.config();
 
 app.use(express.json()); //to parse the incoming request from json payloads(from req.body of auth.controller)
@@ -20,6 +21,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/user", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 // app.get('/',(req,res)=>{
 //root route http://localhost:5000
 //     res.send("Hello");
